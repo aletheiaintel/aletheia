f658356e4a61188e1d5fe91510d22de4d0956123
Omotola Jinadu <91120863+tolagenius1123@users.noreply.github.com>
2026-04-17 20:15:16 +0100

    project setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,12 @@
 import type { Metadata } from "next";
-import { Poppins, Geist } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
-
-const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 const poppins = Poppins({
 	subsets: ["latin"],
 	weight: ["300", "400", "500", "600", "700"],
-	variable: "--font-poppins",
+	variable: "--font-sans",
 });
 
 export const metadata: Metadata = {
@@ -18,11 +16,11 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
 	children,
-}: Readonly<{
+}: {
 	children: React.ReactNode;
-}>) {
+}) {
 	return (
-		<html lang="en" className={cn("h-full", "antialiased", poppins.variable, "font-sans", geist.variable)}>
+		<html lang="en" className={cn("h-full antialiased", poppins.variable)}>
 			<body className="min-h-full flex flex-col">{children}</body>
 		</html>
 	);
